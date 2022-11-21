572dd65ea82d34f6564720a852981e9884c78f87
Richard Gill <richard@rgill.co.uk>
2022-11-21 14:37:28 +0000

    fix

--- src/server/analytics.ts
+++ src/server/analytics.ts
@@ -1,7 +1,7 @@
 import { Express } from 'express'
 import { createProxyMiddleware } from 'http-proxy-middleware'
 
-export const configureAnalytics = (app: Express) => {
+export const configureAnalyticsProxy = (app: Express) => {
   app.use(
     '/mp',
     createProxyMiddleware({
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config'
 import express from 'express'
 import { configureWeb } from '~/server/web'
-import { configureAnalytics } from './analytics'
+import { configureAnalyticsProxy } from './analytics'
 import { configureTrpc } from './trpc'
 import { configureWebhooks } from './webhooks'
 
 const startServer = async (): Promise<void> => {
   let app = express()
 
-  app = configureAnalytics(app)
+  app = configureAnalyticsProxy(app)
   app = configureWebhooks(app)
   app = configureTrpc(app)
   app = await configureWeb(app)
