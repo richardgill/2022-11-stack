ae0b1f0f38b957486d8fd520ae711cde2b2a1709
Richard Gill <richard@rgill.co.uk>
2022-11-23 08:58:38 +0000

    removed console.log

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -2,7 +2,6 @@ export const Form = (props: React.FormHTMLAttributes<HTMLFormElement>) => (
   <form
     {...props}
     onSubmit={(event) => {
-      console.log('onsubmit')
       event.preventDefault()
       if (props.onSubmit) {
         props.onSubmit(event)
