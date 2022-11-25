cd496fe79a1ff24e3f7df96efbc05722e5cb4a5c
Richard Gill <richard@rgill.co.uk>
2022-11-25 08:17:24 +0000

    fixes

--- packages/web/src/utils/environmentVariables.ts
+++ packages/web/src/utils/environmentVariables.ts
@@ -1,2 +1,4 @@
 export const baseUrl =
-  process.env.RENDER_EXTERNAL_URL ?? process.env.BASE_URL ?? ''
+  process.env.IS_PULL_REQUEST === 'true'
+    ? process.env.RENDER_EXTERNAL_URL ?? ''
+    : process.env.BASE_URL ?? process.env.RENDER_EXTERNAL_URL ?? ''
