babc2ab87b2ecae0fae1aaedba813e5d1344242d
Richard Gill <richard@rgill.co.uk>
2022-12-02 10:44:21 +0000

    admin client side

--- packages/web/src/pages/admin/index.page.tsx
+++ packages/web/src/pages/admin/index.page.tsx
@@ -1,8 +1,14 @@
+import { Link } from '~/components/link'
 import { trpcReact } from '~/utils/trpc'
 
 export const Page = () => {
   const { data: adminStuff } = trpcReact.admin.getAdminStuff.useQuery()
-  return <div>Admin Stuff!{JSON.stringify(adminStuff)}</div>
+  return (
+    <div>
+      Admin Stuff!<Link href="/admin/clientSide">Client Admin</Link>
+      {JSON.stringify(adminStuff)}
+    </div>
+  )
 }
 
 export const documentProps = { title: 'Taxpal: Admin page' }
